422d9eb9e8da4f208a1b0a2ac53cb937d9022ee6
heno-1010 <heno-1010@users.noreply.github.com>
2026-05-31 17:58:13 +0900

    Add live preview text binding

--- XAMLens/ViewModels/MainWindowViewModel.cs
+++ XAMLens/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,24 @@
-﻿namespace XAMLens.ViewModels
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace XAMLens.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
+        public string _xamlText;
 
+        public string XAMLText
+        {
+            get => _xamlText;
+            set
+            {
+                if (_xamlText != value)
+                {
+                    _xamlText = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
     }
 }
--- XAMLens/Views/MainWindow.axaml.cs
+++ XAMLens/Views/MainWindow.axaml.cs
@@ -7,6 +7,7 @@ namespace XAMLens.Views
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new ViewModels.MainWindowViewModel();
         }
     }
 }
